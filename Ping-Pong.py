9af15753746ae40fa2fc415934be3f94f1ad9734
artem2756 <118670063+artem2756@users.noreply.github.com>
2022-11-27 17:10:46 +0300

    v2.0
    Игра полностью доделана. Удачи в игре!

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -38,20 +38,42 @@ player1 = Player("ракетка.png", 50, 0, 50, 200, 10)
 player2 = Player("ракетка.png", 800, 0, 50, 200, 10)
 ball = GameSprite('ball.png', 200, 200, 60, 60, 60)
 
+speed_x = 3
+speed_y = 3
+
+font.init()
+font = font.SysFont('Georgia',36)
+text_won = font.render("Левый проиграл", True, (0,0,0))
+text_win = font.render("Правый проиграл", True, (0,0,0))
+
 clock = time.Clock()
 FPS = 60
 game = True
+finish = False
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    w.fill(background)
-    player1.reset()
-    player1.update_l()
-    player2.reset()
-    player2.update_p()
-    ball.reset()
-    ball.update()
+    if finish != True:
+        w.fill(background)
+        player1.reset()
+        player1.update_l()
+        player2.reset()
+        player2.update_p()
+        ball.reset()
+        ball.update()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > win_height - 58 or ball.rect.y <0:
+            speed_y *= -1
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            speed_x *= -1
+        if ball.rect.x <0:
+            finish = True
+            w.blit(text_won, (310,350))
+        if ball.rect.x > win_width:
+            finish = True
+            w.blit(text_win, (310,350))
     display.update()
     clock.tick(FPS)
