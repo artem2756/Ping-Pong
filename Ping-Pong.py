eafa48040fd057d654dcd2db94ff9ee1963b722b
artem2756 <118670063+artem2756@users.noreply.github.com>
2022-11-27 16:09:24 +0300

    v1.3
    Добавлены ракетки на фон игры.

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -1,15 +1,15 @@
 from pygame import *
 from random import randint
 from time import time as timer
-win_width = 1820
-win_height = 1080
+win_width = 900
+win_height = 700
 w = display.set_mode((win_width, win_height))
 display.set_caption('Пинг-Понг')
 background = (200, 255, 255)
 w.fill(background)
 
 class GameSprite(sprite.Sprite):
-    def __init__ (self,player_image,player_x,player_y,size_x,size_y,player_speed,):
+    def __init__ (self,player_image,player_x,player_y,size_x,size_y,player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
@@ -20,13 +20,24 @@ class GameSprite(sprite.Sprite):
         w.blit(self.image, (self.rect.x,self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_a] and self.rect.x>0:
-            self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x<625:
-            self.rect.x += self.speed
-        
+        if keys[K_w] and self.rect.y>0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y<625:
+            self.rect.y += self.speed
+    def update_p(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y>0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y<625:
+            self.rect.y += self.speed
+
+
+player1 = Player("ракетка.png", 50, 0, 50, 200, 10)
+player2 = Player("ракетка.png", 800, 0, 50, 200, 10)
+ball = GameSprite
+
 clock = time.Clock()
 FPS = 60
 game = True
@@ -35,5 +46,10 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    w.fill(background)
+    player1.reset()
+    player1.update_l()
+    player2.reset()
+    player2.update_p()
     display.update()
     clock.tick(FPS)
